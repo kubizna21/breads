a14fb12457032be24534b4e6ac3b1495aedcfd4f
Ashley Kubizna <113566558+kubizna21@users.noreply.github.com>
2023-03-14 20:18:52 -0700

    added to breadscontroller

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -28,4 +28,20 @@ breads_router.get('/', (req, res) => {
     //res.send(Bread)
 })
 
+breads_router.post('/', (req, res) => {
+    // req.body.hasGluten = req.body.hasGluten === 'on'
+
+    if (req.body.hasGluten === 'on') {
+        req.bodyhasGluten = 'true'
+    } else {
+        req.body.hasGluten = 'false'
+
+    }
+
+    Bread.push(req.body)
+    res.send(Bread)
+
+    })
+})
+
 module.exports = breads_router
